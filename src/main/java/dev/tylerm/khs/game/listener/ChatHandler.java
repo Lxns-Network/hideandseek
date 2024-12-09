6498ac538e536ce3a1aca6869dabd7c7ad2e3572
iceBear67 <icebear67@sfclub.cc>
2024-12-09 21:38:53 +0800

    add chat

--- src/main/java/dev/tylerm/khs/game/listener/ChatHandler.java
+++ src/main/java/dev/tylerm/khs/game/listener/ChatHandler.java
@@ -1,6 +1,8 @@
 package dev.tylerm.khs.game.listener;
 
 import dev.tylerm.khs.Main;
+import dev.tylerm.khs.game.util.Status;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -11,9 +13,25 @@ public class ChatHandler implements Listener {
 
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onChat(AsyncPlayerChatEvent event) {
-        if (Main.getInstance().getBoard().isSpectator(event.getPlayer())) {
-            event.setCancelled(true);
-            Main.getInstance().getBoard().getSpectators().forEach(spectator -> spectator.sendMessage(ChatColor.GRAY + "[SPECTATOR] " + event.getPlayer().getName() + ": " + event.getMessage()));
+        event.setCancelled(true);
+        //todo add config for chat configuration
+        var board = Main.getInstance().getBoard();
+        if (board.isSpectator(event.getPlayer())) {
+            board.getSpectators().forEach(spectator -> spectator.sendMessage(ChatColor.GRAY + "[SPECTATOR] " + event.getPlayer().getName() + ": " + event.getMessage()));
+        } else {
+            var status = Main.getInstance().getGame().getStatus();
+            if (status == Status.PLAYING) {
+                Bukkit.broadcastMessage(
+                        ChatColor.BOLD +
+                                (board.isHider(event.getPlayer()) ?
+                                        ChatColor.GOLD + "[BLOCK] "
+                                        : ChatColor.BLUE + "SEEKER ")
+                                + ChatColor.WHITE + event.getPlayer().getName() + ": " + ChatColor.GRAY + event.getMessage());
+            } else {
+                Bukkit.broadcastMessage(
+                        event.getPlayer().getName() + ": " + ChatColor.GRAY + event.getMessage()
+                );
+            }
         }
     }
 
